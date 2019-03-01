2cd3bd113ecd29ad7360b04c3b04964649f2c725
Tyler Long <tyler4long@gmail.com>
2019-03-01 15:55:09 +0800

    Try to generate code

--- index.ts
+++ index.ts
@@ -1,6 +1,23 @@
 import * as fs from 'fs'
 import * as yaml from 'js-yaml'
+import * as changeCase from 'change-case'
 
-const doc = yaml.safeLoad(fs.readFileSync(process.env.SWAGGER_FILE_PATH, 'utf8'))
+const doc = yaml.safeLoad(fs.readFileSync(process.env.SWAGGER_FILE_PATH, 'utf8'));
 
-console.log(doc)
+console.log(doc);
+
+const paths = Object.keys(doc.paths);
+
+let markdown = '# RingCentral JS SDK code samples';
+paths.forEach(path => {
+  const methods = Object.keys(doc.paths[path]);
+  methods.forEach(method => {
+    const operation = doc.paths[path][method];
+    console.log(path, method);
+    markdown += `\n\n## ${changeCase.sentenceCase(operation.operationId)}
+
+HTTP ${changeCase.upperCase(method)} ${path}`
+  });
+});
+
+console.log(markdown);
