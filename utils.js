10a2c0169f5e1562193c76d93b484831ef6076ff
Tyler Long <tyler4long@gmail.com>
2019-03-12 10:02:12 +0800

    First version of loadFullDefinition

---
+++ utils.js
@@ -0,0 +1,23 @@
+import fs from 'fs'
+import yaml from 'js-yaml'
+import * as R from 'ramda'
+
+const doc = yaml.safeLoad(fs.readFileSync(process.env.SWAGGER_FILE_PATH, 'utf8'))
+
+const loadFullDefinition = name => {
+  const model = R.clone(doc.definitions[name])
+  Object.keys(model.properties).forEach(key => {
+    const prop = model.properties[key]
+    if (prop['$ref']) {
+      prop.properties = loadFullDefinition(R.last(prop['$ref'].split('/'))).properties
+      delete prop['$ref']
+    }
+    if (prop.items && prop.items['$ref']) {
+      prop.items.properties = loadFullDefinition(R.last(prop.items['$ref'].split('/'))).properties
+      delete prop.items['$ref']
+    }
+  })
+  return model
+}
+
+console.log(JSON.stringify(loadFullDefinition('CreateSMSMessage'), null, 2))
